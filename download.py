e94098b42fe14f7915e03b594087549d4f754d71
hajalibayram <hajalibayram@outlook.com>
2026-01-07 18:40:03 +0100

    Script refactoring
    -download.py uses curl

--- download.py
+++ download.py
@@ -1,25 +1,24 @@
 import os
-import urllib.request
+import subprocess
 
-def download(url: str, dest_folder: str):
-    if not os.path.exists(dest_folder):
-        os.makedirs(dest_folder)
+def download_with_curl(url: str, dest_folder: str):
+    os.makedirs(dest_folder, exist_ok=True)
 
     filename = url.split('/')[-1].replace(" ", "_")
     file_path = os.path.join(dest_folder, filename)
 
-    def progress_hook(block_num, block_size, total_size):
-        downloaded = block_num * block_size
-        if total_size > 0:
-            percent = min(downloaded / total_size * 100, 100)
-            print(f"\rDownloading: {percent:6.2f}%", end="")
-        else:
-            print(f"\rDownloaded {downloaded} bytes", end="")
-
     print("Saving to", os.path.abspath(file_path))
-    urllib.request.urlretrieve(url, file_path, reporthook=progress_hook)
+
+    cmd = [
+        "curl",
+        "-L",        # follow redirects (Zenodo needs this)
+        "-C", "-",   # resume
+        "-o", os.path.join(dest_folder, url.split("/")[-1]),
+        url
+    ]
+
+    subprocess.run(cmd, check=True)
     print("\nDownload complete.")
 
-
-download('https://zenodo.org/records/14949245/files/metadata.csv', 'data')
-download('https://zenodo.org/records/14949245/files/imputed_goiener_v7.tar.zst', 'data')
+download_with_curl('https://zenodo.org/records/14949245/files/metadata.csv', 'data')
+download_with_curl('https://zenodo.org/records/14949245/files/imputed_goiener_v7.tar.zst', 'data')
